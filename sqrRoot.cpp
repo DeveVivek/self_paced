e92e1f2d7b342505fe2f0141566fdbcdc2a0f241
Vivek Kumar <code.vivekkumar@gmail.com>
2022-09-16 22:16:14 +0530

    square root

--- sqrRoot.cpp
+++ sqrRoot.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int square(int n){
+int square(int n)
+{
     int count = 0;
-    for(int i = 1; i*i<n ; i++){
+    for (int i = 1; i * i < n; i++)
+    {
         count++;
     }
     return count;
@@ -11,8 +13,8 @@ int square(int n){
 
 int main()
 {
-    int n ;
-    cin>>n;
-    cout<<square(n);
+    int n;
+    cin >> n;
+    cout << square(n);
     return 0;
 }
