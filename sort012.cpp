015f9e0086c748ece8fb557cad8e8eb1e2b19ce4
Vivek Kumar <code.vivekkumar@gmail.com>
2022-09-17 17:49:45 +0530

    sort 012

--- sort012.cpp
+++ sort012.cpp
@@ -1,39 +1,46 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void sort012(int arr[], int n){
-    int i, temp , next0=0, next2=n-1;
-    for(i = 0; i<=next2;){
-        if(arr[i]==2){
+void sort012(int arr[], int n)
+{
+    int i, temp, next0 = 0, next2 = n - 1;
+    for (i = 0; i <= next2;)
+    {
+        if (arr[i] == 2)
+        {
             temp = arr[i];
             arr[i] = arr[next2];
             arr[next2] = temp;
             next2--;
         }
-        else if(arr[i]==0){
-            temp= arr[i];
-            arr[i]=arr[next0];
-            arr[next0]=temp;
+        else if (arr[i] == 0)
+        {
+            temp = arr[i];
+            arr[i] = arr[next0];
+            arr[next0] = temp;
             next0++;
             i++;
         }
-        else{
+        else
+        {
             i++;
         }
     }
 }
 
-void printArray(int arr[], int n){
-    for(int i = 0; i<n; i++){
-        cout<<arr[i]<<" ";
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
 
 int main()
 {
-    int arr[10]={1,2,0,0,0,1,2,2,0,1};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    sort012(arr,n);
-    printArray(arr,n);
+    int arr[10] = {1, 2, 0, 0, 0, 1, 2, 2, 0, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort012(arr, n);
+    printArray(arr, n);
     return 0;
 }
