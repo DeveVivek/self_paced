4ae3ec1313dfacf0d6f00dec162c807d38950d9d
Vivek Kumar <code.vivekkumar@gmail.com>
2022-09-18 21:58:57 +0530

    duplicates in array

--- duplicatesInArr.cpp
+++ duplicatesInArr.cpp
@@ -22,9 +22,9 @@ int finDup(int arr[], int bot, int top)
 
 int main()
 {
-    int arr[] = {1,2,2,3};
+    int arr[] = {1, 2, 2, 3};
     int size = sizeof(arr) / sizeof(arr[0]);
     int out = finDup(arr, 0, size);
-    cout<<out<<" ";
+    cout << out << " ";
     return 0;
 }
