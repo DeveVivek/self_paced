584217db71e7580ec3c1deb266e23e15b7765beb
Vivek Kumar <code.vivekkumar@gmail.com>
2022-09-25 19:59:14 +0530

    tables

--- tables.cpp
+++ tables.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main()
 {
     int i, n;
-    cin>>n;
+    cin >> n;
     for (int i = 1; i <= 10; i++)
     {
-        int table = n*i;
-            cout<< n << "*" << i << " = " << table<<endl;
+        int table = n * i;
+        cout << n << "*" << i << " = " << table << endl;
     }
     return 0;
 }
